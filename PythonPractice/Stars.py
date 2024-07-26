75e0072056eba0244ffd4f9f68d78da1c63af8fc
Akram Hammam <63311262+akramhammam5@users.noreply.github.com>
2024-07-26 19:19:11 +0300

    Playing with asterisks
    A simple code that replaces asterisks by another values

---
+++ PythonPractice/Stars.py
@@ -0,0 +1,19 @@
+#Considering this as a GUI Image
+picture = [
+  [0,0,0,1,0,0,0],
+  [0,0,1,1,1,0,0],
+  [0,1,1,1,1,1,0],
+  [1,1,1,1,1,1,1],
+  [0,0,0,1,0,0,0],
+  [0,0,0,1,0,0,0],
+]
+
+
+for i in picture:
+  for j in i:
+     if j == 1:
+      print('*',end='')
+     else:
+      print(' ',end='')
+  print('\n')
+
