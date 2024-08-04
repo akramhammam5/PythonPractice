7b39afb038cf7f9f22df3d334c05e4fdb77f6d8a
Akram Hammam <63311262+akramhammam5@users.noreply.github.com>
2024-08-04 20:26:03 +0300

    How many apples ?
    How many apples are given to how many persons ?

---
+++ PythonPractice/Apples.py
@@ -0,0 +1,4 @@
+number_of_apples = input()
+numbers_of_persons = input()
+apples_given_to_each = number_of_apples // numbers_of_persons
+apples_left = number_of_apples - (apples_given_to_each * numbers_of_persons)
