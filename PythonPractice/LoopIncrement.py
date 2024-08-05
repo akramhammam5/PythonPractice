0dd2963350c64bbdb271c1e40a62a0291266119f
Akram Hammam <63311262+akramhammam5@users.noreply.github.com>
2024-08-05 16:29:25 +0300

    Increment i in list
    Increment each i in list j :)

---
+++ PythonPractice/LoopIncrement.py
@@ -0,0 +1,8 @@
+values = [16, 1, 7, 2, 19, 12, 5, 20, 2, 10, 10, 14, 17, 14, 1, 16, 19, 7, 9, 19]
+values2 = []
+for i in values:
+    i+=1
+    values2.append(i)
+values = values2
+print(values)
+    
